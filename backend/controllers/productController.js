2dcf570eb2d275211cbc537289d0cb36146aa0b6
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-10-03 09:52:30 +0800

    Find by Product ID and Delete

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -68,14 +68,14 @@ exports.updateProduct = async (req, res, next) => {
 }
 
 exports.deleteProduct = async (req, res, next) => {
-    const product = await Product.findById(req.params.id);
+    const product = await Product.findByIdAndDelete(req.params.id);
     if (!product) {
         return res.status(404).json({
             success: false,
             message: 'Product not found'
         })
     }
-    await product.remove();
+    // await product.remove();
     res.status(200).json({
         success: true,
         message: 'Product deleted'
