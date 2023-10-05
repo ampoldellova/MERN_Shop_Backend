3e5b93ce01c69126e2248d96c45512c2c248de70
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-10-05 09:59:28 +0800

    Register User

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const products = require('./routes/product');
+const auth = require('./routes/auth');
 
 app.use(express.json());
 
 app.use('/api/v1', products);
+app.use('/api/v1', auth);
 
 module.exports = app
