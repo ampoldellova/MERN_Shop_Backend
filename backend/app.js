2f01de3a226cf6f8a6b7154e940a1a7cac9765a9
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-10-05 11:16:29 +0800

    Middleware Authentication

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const products = require('./routes/product');
 const auth = require('./routes/auth');
+const cookieParser = require('cookie-parser')
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/v1', products);
 app.use('/api/v1', auth);
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController');
+const { isAuthenticatedUser } = require('../middlewares/auth');
 
 router.get('/products', getProducts)
-router.post('/product/new', newProduct)
+router.post('/product/new', isAuthenticatedUser, newProduct)
 router.get('/product/:id', getSingleProduct);
-router.route('/admin/product/:id',).put(updateProduct).delete(deleteProduct);
+router.route('/admin/product/:id', isAuthenticatedUser).put(updateProduct).delete(deleteProduct);
 
 module.exports = router;
