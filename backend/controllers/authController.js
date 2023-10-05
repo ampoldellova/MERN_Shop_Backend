178950a46f385e12c3e11e997027b3b00aa6e8b7
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-10-05 11:49:36 +0800

    Middleware AuthorizeRoles

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,7 +2,7 @@ const User = require('../models/users');
 const sendToken = require('../utils/jwtToken');
 
 exports.registerUser = async (req, res, next) => {
-    const { name, email, password } = req.body;
+    const { name, email, password, role } = req.body;
     const user = await User.create({
         name,
         email,
@@ -11,15 +11,15 @@ exports.registerUser = async (req, res, next) => {
             public_id: 'avatars/hsdfl66pg2mpvp5irfqy',
             url: 'https://res.cloudinary.com/dgneiaky7/image/upload/v1680144230/avatars/hsdfl66pg2mpvp5irfqy.jpg'
         },
-        // role,
+        role,
     })
     //test token
-    const token = user.getJwtToken();
-    res.status(201).json({
-        success: true,
-        user,
-        token
-    })
+    // const token = user.getJwtToken();
+    // res.status(201).json({
+    //     success: true,
+    //     user,
+    //     token
+    // })
     sendToken(user, 200, res)
 }
 
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController');
-const { isAuthenticatedUser } = require('../middlewares/auth');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 router.get('/products', getProducts)
-router.post('/product/new', isAuthenticatedUser, newProduct)
+router.post('/product/new', isAuthenticatedUser, authorizeRoles('admin'), newProduct)
 router.get('/product/:id', getSingleProduct);
-router.route('/admin/product/:id', isAuthenticatedUser).put(updateProduct).delete(deleteProduct);
+router.route('/admin/product/:id', isAuthenticatedUser, authorizeRoles('admin')).put(updateProduct).delete(deleteProduct);
 
 module.exports = router;
