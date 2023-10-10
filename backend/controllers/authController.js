86a6221612d3a6f8a11fe0ab895432fbfc47a63d
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-10-10 08:31:31 +0800

    Send Email

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -107,3 +107,29 @@ exports.forgotPassword = async (req, res, next) => {
         // return next(new ErrorHandler(error.message, 500))
     }
 }
+
+exports.resetPassword = async (req, res, next) => {
+    // Hash URL token
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.token).digest('hex')
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() }
+    })
+
+    if (!user) {
+        return res.status(400).json({ message: 'Password reset token is invalid or has been expired' })
+        // return next(new ErrorHandler('Password reset token is invalid or has been expired', 400))
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+        return res.status(400).json({ message: 'Password does not match' })
+        // return next(new ErrorHandler('Password does not match', 400))
+    }
+
+    // Setup new password
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save();
+    sendToken(user, 200, res);
+}
