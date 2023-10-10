f248d4320c481a97ea818dac245b5845c18b16d3
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-10-10 09:47:23 +0800

    Get All Users

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const { isAuthenticatedUser, authorizeRoles } = require ('../middlewares/auth');
-const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserProfile } = require('../controllers/authController');
+const { isAuthenticatedUser,
+    authorizeRoles } = require('../middlewares/auth');
+const { registerUser,
+    loginUser,
+    logout,
+    forgotPassword,
+    resetPassword,
+    getUserProfile,
+    updatePassword,
+    updateProfile,
+    allUsers } = require('../controllers/authController');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/logout', logout);
-
 router.post('/password/forgot', forgotPassword);
 router.put('/password/reset/:token', resetPassword);
 router.get('/me', isAuthenticatedUser, getUserProfile)
+router.put('/password/update', isAuthenticatedUser, updatePassword)
+router.put('/me/update', isAuthenticatedUser, updateProfile)
+router.get('/admin/users', isAuthenticatedUser, allUsers)
 
 module.exports = router;
