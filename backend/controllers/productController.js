58c620c027fed510bc1c5b6ce65a5eed8d10abfc
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-10-12 21:51:38 +0800

    pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,8 +1,6 @@
 const Product = require('../models/product')
 const APIFeatures = require('../utils/apiFeatures');
 
-
-//create new product
 exports.newProduct = async (req, res, next) => {
 	
 	req.body.user = req.user.id;
@@ -13,72 +11,83 @@ exports.newProduct = async (req, res, next) => {
 	})
 }
 
-exports.getProducts = async (req, res, next) => {
+// exports.getProducts = async (req, res, next) => {
+// 	const products = await Product.find({});
+// 	res.status(200).json({
+// 		success: true,
+// 		count: products.length,
+// 		products
+// 	})
+// }
 
-    const resPerPage = 4;
-    const productsCount = await Product.countDocuments();
-    const apiFeatures = new APIFeatures(Product.find(), req.query).search().filter()
-    // .filter(); 
+http://localhost:4001/api/v1/products?keyword=apple&page=2
+exports.getProducts = async (req,res,next) => {
+	
+	const resPerPage = 4;
+	const productsCount = await Product.countDocuments();
+	const apiFeatures = new APIFeatures(Product.find(),req.query).search().filter(); 
 
-    // const products = await Product.find();
-    apiFeatures.pagination(resPerPage);
-    const products = await apiFeatures.query;
-    res.status(200).json({
-        success: true,
-        count: products.length,
-        products
-    })
+	// const products = await Product.find();
+	apiFeatures.pagination(resPerPage);
+	const products = await apiFeatures.query;
+	res.status(200).json({
+		success: true,
+		count: products.length,
+		productsCount,
+		products,
+		resPerPage,
+	})
 }
 
 exports.getSingleProduct = async (req, res, next) => {
-    const product = await Product.findById(req.params.id);
-    if (!product) {
-        return res.status(404).json({
-            success: false,
-            message: 'Product not found'
-        })
-    }
-    res.status(200).json({
-        success: true,
-        product
-    })
+	const product = await Product.findById(req.params.id);
+	if (!product) {
+		return res.status(404).json({
+			success: false,
+			message: 'Product not found'
+		})
+	}
+	res.status(200).json({
+		success: true,
+		product
+	})
 }
 
 exports.updateProduct = async (req, res, next) => {
-    let product = await Product.findById(req.params.id);
-    console.log(req.body)
-    if (!product) {
-        return res.status(404).json({
-            success: false,
-            message: 'Product not found'
-        })
-    }
-    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-    })
-    if (!product) {
-        return res.status(404).json({
-            success: false,
-            message: 'Product not updated'
-        })
-    }
-    res.status(200).json({
-        success: true,
-        product
-    })
+	let product = await Product.findById(req.params.id);
+	console.log(req.body)
+	if (!product) {
+		return res.status(404).json({
+			success: false,
+			message: 'Product not found'
+		})
+	}
+	product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+	})
+	if (!product) {
+		return res.status(404).json({
+			success: false,
+			message: 'Product not updated'
+		})
+	}
+	res.status(200).json({
+		success: true,
+		product
+	})
 }
 
 exports.deleteProduct = async (req, res, next) => {
-    const product = await Product.findByIdAndDelete(req.params.id);
-    if (!product) {
-        return res.status(404).json({
-            success: false,
-            message: 'Product not found'
-        })
-    }
-    // await product.remove();
-    res.status(200).json({
-        success: true,
-        message: 'Product deleted'
-    })
+	const product = await Product.findByIdAndDelete(req.params.id);
+	if (!product) {
+		return res.status(404).json({
+			success: false,
+			message: 'Product not found'
+		})
+	}
+	// await product.remove();
+	res.status(200).json({
+		success: true,
+		message: 'Product deleted'
+	})
 }
