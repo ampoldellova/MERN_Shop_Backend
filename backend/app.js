54698d9b4760925a9d819a2cc7a5e6626cd4b051
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-10-12 10:43:31 +0800

    Home

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const products = require('./routes/product');
 const auth = require('./routes/auth');
+const order = require('./routes/order');
+const cors = require('cors')
 const cookieParser = require('cookie-parser')
 
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/v1', products);
 app.use('/api/v1', auth);
+app.use('/api/v1', order);
 
 module.exports = app
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController');
+const {newProduct, getProducts, getSingleProduct, updateProduct, deleteProduct} = require('../controllers/productController');
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
-
 router.get('/products', getProducts)
-// router.post('/product/new', isAuthenticatedUser, authorizeRoles('admin'), newProduct)
+
 router.post('/product/new', isAuthenticatedUser, newProduct)
-// router.post('/product/new', newProduct)
 router.get('/product/:id', getSingleProduct);
-router.route('/admin/product/:id', isAuthenticatedUser, authorizeRoles('admin')).put(updateProduct).delete(deleteProduct);
+router.route('/admin/product/:id', isAuthenticatedUser, authorizeRoles('admin',)).put(updateProduct).delete(deleteProduct);
 
 module.exports = router;
